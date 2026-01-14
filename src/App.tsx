a6cb521c5165a4775ef3b0ddc21b6a7b4c03f1e5
Marcos santos <blackmaxsuke0220@gmail.com>
2026-01-14 18:59:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './assets/components/footer/Footer'
+import Navbar from './assets/components/navbar/Navbar'
+import Home from './pages/home/Home'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,27 @@
+function Home() {
+  return (
+    <>
+      <main className="bg-indigo-900 flex justify-center">
+        <section className="container grid grid-cols-2 text-white">
+          <article className="flex flex-col gap-4 items-center justify-center py-4">
+             <h1 className="text-5xl font-bold">
+              Seja Bem Vinde!</h1>
+             <p className="text-xl">
+              Expresse aqui seus pensamentos e opiniões</p>
+
+              <div className="flex justify-center gap-4">
+              <button className="rounded text-white border-white border-solid
+                                 border-2 py-2 px-4">
+                  Nova Postagem
+                  </button>
+              </div>
+          </article>
+          <figure className="flex justify-center">
+            <img src="https://i.imgur.com/fyfri1v.png" alt="" className="w-2/3"/>
+          </figure>
+        </section>
+      </main>
+    </>
+  );
+}
+export default Home;
