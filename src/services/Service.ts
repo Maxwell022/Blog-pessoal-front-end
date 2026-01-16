1920b1c89f0f494d979df3a9d503ea4f1ed74b50
Marcos santos <blackmaxsuke0220@gmail.com>
2026-01-15 22:56:16 -0300

    feat:backEnd cadastrar

---
+++ src/services/Service.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://blog-pessoal-mi8e.onrender.com'
+})
+
+export const cadastrarUsuario = async(url: string, dados: object,setDados: Function) =>{
+    console.log("entrou")
+    const resposta = await api.post(url, dados);
+    setDados(resposta.data)
+}
+
+export const login = async(url: string, dados: object,setDados: Function) => {
+    const resposta = await api.post(url, dados);
+    setDados(resposta.data)
+}
