ab3dab645fab419d72e11d279cde4309f9a90908
Marcos santos <blackmaxsuke0220@gmail.com>
2026-01-16 12:24:32 -0300

    feat:backEnd do login

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,24 @@ import Navbar from './assets/components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Navbar/>
-      <div className='min-h-[80vh]'>
-        <Routes>
-          <Route path='/' element={<Login/>}/>
-          <Route path='/cadastro' element={<Cadastro/>}/>
-          <Route path='/home' element={<Home/>}/>
-        </Routes>
-      </div>
-      <Footer/>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar/>
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path='/' element={<Login/>}/>
+            <Route path='/cadastro' element={<Cadastro/>}/>
+            <Route path='/home' element={<Home/>}/>
+          </Routes>
+        </div>
+        <Footer/>
+      </BrowserRouter>
+    </AuthProvider>
     </>
   )
 }
